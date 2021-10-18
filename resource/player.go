e9bd52ed79ab3c79deae39aff3e42981ea10d564
Jackie <18378976+Pyrodash@users.noreply.github.com>
2021-10-18 18:58:53 +0200

    Added Player State type

--- resource/player.go
+++ resource/player.go
@@ -1,5 +1,7 @@
 package resource
 
+import "time"
+
 type MediaItemInfo struct {
 	Title string `json:"title" redis:"title" msgpack:"title"`
 	Icon string `json:"icon" redis:"icon" msgpack:"icon"`
@@ -9,4 +11,10 @@ type MediaItemInfo struct {
 type MediaItem struct {
 	Id string `json:"id" redis:"id" msgpack:"id"`
 	*MediaItemInfo
+}
+
+type PlayerState struct {
+	CurrentTime time.Time `json:"current_time" redis:"current_time" msgpack:"current_time"`
+	IsPlaying bool `json:"playing" redis:"playing" msgpack:"playing"`
+	PlaybackStart time.Time `json:"-" redis:"playback_start" msgpack:"playback_start"`
 }
