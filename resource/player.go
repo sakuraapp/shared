b3a9ba8d1f95141309ddba6d9e4b69d8fc9dbfcd
Jackie <18378976+Pyrodash@users.noreply.github.com>
2021-10-18 03:34:26 +0200

    Added MediaItem type

---
+++ resource/player.go
@@ -0,0 +1,12 @@
+package resource
+
+type MediaItemInfo struct {
+	Title string `json:"title" redis:"title" msgpack:"title"`
+	Icon string `json:"icon" redis:"icon" msgpack:"icon"`
+	Url string `json:"url" redis:"url" msgpack:"url"`
+}
+
+type MediaItem struct {
+	Id string `json:"id" redis:"id" msgpack:"id"`
+	MediaItemInfo
+}
