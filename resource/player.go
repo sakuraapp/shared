6257121de29c85343e8d67a0c8c649d25900761d
Jackie <18378976+Pyrodash@users.noreply.github.com>
2021-11-04 14:20:11 +0200

    Removed BuildPacket method from PlayerState

--- resource/player.go
+++ resource/player.go
@@ -2,7 +2,6 @@ package resource
 
 import (
 	"github.com/sakuraapp/shared/model"
-	"github.com/sakuraapp/shared/resource/opcode"
 	"time"
 )
 
@@ -22,8 +21,4 @@ type PlayerState struct {
 	CurrentTime time.Duration `json:"currentTime" redis:"currentTime" msgpack:"currentTime"`
 	IsPlaying bool `json:"playing" redis:"playing" msgpack:"playing"`
 	PlaybackStart time.Time `json:"-" redis:"playbackStart" msgpack:"playbackStart"`
-}
-
-func (s *PlayerState) BuildPacket() Packet {
-	return BuildPacket(opcode.PlayerState, s)
 }
