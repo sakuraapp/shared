08b2722ce75d1e93b61a755829ae179d15d452af
Jackie <18378976+Pyrodash@users.noreply.github.com>
2022-01-07 19:05:35 +0200

    Implemented RoleManager

--- resource/role/role.go
+++ resource/role/role.go
@@ -19,3 +19,55 @@ var permissions = map[Role]permission.Permission{
 func (r Role) Permissions() permission.Permission {
 	return permissions[r]
 }
+
+type RoleManager struct {
+	roles map[Role]bool
+	permissions permission.Permission
+}
+
+func (m *RoleManager) recalculatePerms() {
+	m.permissions = 0
+
+	for role := range m.roles {
+		m.permissions |= role.Permissions()
+	}
+}
+
+func (m *RoleManager) Add(role Role) {
+	m.roles[role] = true
+	m.permissions |= role.Permissions()
+}
+
+func (m *RoleManager) Remove(role Role) {
+	delete(m.roles, role)
+	m.recalculatePerms()
+}
+
+func (m *RoleManager) Has(role Role) bool {
+	return m.roles[role]
+}
+
+func (m *RoleManager) Roles() []Role {
+	roles := make([]Role, len(m.roles))
+
+	for role := range m.roles {
+		roles = append(roles, role)
+	}
+
+	return roles
+}
+
+func (m *RoleManager) Permissions() permission.Permission {
+	return m.permissions
+}
+
+func (m *RoleManager) HasPermission(perm permission.Permission) bool {
+	return m.permissions.Has(perm)
+}
+
+func NewRoleManager() *RoleManager {
+	return &RoleManager{
+		roles: map[Role]bool{},
+		permissions: 0,
+	}
+}
