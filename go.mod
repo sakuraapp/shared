f28ac6c1d11492ead5fcd19f8acc8edcd7172082
Pyrodash <18378976+Pyrodash@users.noreply.github.com>
2023-03-13 18:56:28 +0200

    Implemented some helper types and functions for the new pubsub API

--- go.mod
+++ go.mod
@@ -1,13 +1,21 @@
 module github.com/sakuraapp/shared
 
-go 1.16
+go 1.18
+
+replace github.com/sakuraapp/pubsub => /Users/jackie/Documents/Projects/Sakura/Source/pubsub
 
 require (
 	github.com/go-chi/jwtauth/v5 v5.0.2
 	github.com/go-chi/render v1.0.1
 	github.com/go-redis/redis/v8 v8.11.4
 	github.com/lestrrat-go/jwx v1.2.18
-	github.com/sakuraapp/pubsub v0.0.0-20221113190712-0b5a16cfa4c6 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5
 	gopkg.in/guregu/null.v4 v4.0.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/sakuraapp/pubsub v0.0.0-20230313165424-054ed0eba8a5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 )
--- pkg/dispatcher/gateway/dispatcher.go
+++ pkg/dispatcher/gateway/dispatcher.go
@@ -1,18 +1,105 @@
 package gateway
 
 import (
+	"fmt"
 	"github.com/sakuraapp/pubsub"
+	"github.com/sakuraapp/shared/pkg/constant"
 	"github.com/sakuraapp/shared/pkg/model"
+	"github.com/sakuraapp/shared/pkg/resource"
+	"github.com/sakuraapp/shared/pkg/resource/permission"
 )
 
-type Dispatcher struct {
-	pubsub.Dispatcher
+type Message = pubsub.Message[resource.Packet]
+type SubscriptionManager = pubsub.SubscriptionManager[*Message]
+
+func NewMessage(payload resource.Packet, opts ...pubsub.FilterMap) *Message {
+	return pubsub.NewMessage[resource.Packet](payload, opts...)
 }
 
-func (d *Dispatcher) DispatchToRoom(id model.RoomId, message pubsub.Message) error {
-	return d.DispatchTo(NewRoomTarget(id), message)
+func NewSubscriptionManager(client pubsub.Client) *SubscriptionManager {
+	return pubsub.NewSubscriptionManager[*Message](client)
 }
 
-func NewDispatcher(dispatcher pubsub.Dispatcher) *Dispatcher {
-	return &Dispatcher{dispatcher}
+const (
+	MessageFilterType pubsub.MessageFilterKind = iota
+	MessageFilterIgnoredSession
+	MessageFilterPermissions
+	MessageFilterRoom
+)
+
+type MessageType int
+
+const (
+	NormalMessage MessageType = iota
+	ServerMessage
+)
+
+type FilterMap pubsub.FilterMap
+
+func NewFilterMap() FilterMap {
+	return FilterMap{}
+}
+
+func (m FilterMap) WithType(kind MessageType) FilterMap {
+	m[MessageFilterType] = kind
+
+	return m
+}
+
+func (m FilterMap) WithIgnoredSession(sessionId string) FilterMap {
+	m[MessageFilterIgnoredSession] = sessionId
+
+	return m
+}
+
+func (m FilterMap) WithPermissions(perm permission.Permission) FilterMap {
+	m[MessageFilterPermissions] = perm
+
+	return m
+}
+
+func (m FilterMap) WithRoom(roomId model.RoomId) FilterMap {
+	m[MessageFilterRoom] = roomId
+
+	return m
+}
+
+type MessageTargetNode string
+
+func (t MessageTargetNode) Build() string {
+	return fmt.Sprintf(constant.NodeTopicFmt, t)
+}
+
+func NewNodeTarget(id string) MessageTargetNode {
+	return MessageTargetNode(id)
+}
+
+type MessageTargetSession string
+
+func (t MessageTargetSession) Build() string {
+	return fmt.Sprintf(constant.SessionTopicFmt, t)
+}
+
+func NewSessionTarget(id string) MessageTargetSession {
+	return MessageTargetSession(id)
+}
+
+type MessageTargetUser model.UserId
+
+func (t MessageTargetUser) Build() string {
+	return fmt.Sprintf(constant.UserTopicFmt, t)
+}
+
+func NewUserTarget(id model.UserId) MessageTargetUser {
+	return MessageTargetUser(id)
+}
+
+type MessageTargetRoom model.RoomId
+
+func (t MessageTargetRoom) Build() string {
+	return fmt.Sprintf(constant.RoomTopicFmt, t)
+}
+
+func NewRoomTarget(id model.RoomId) MessageTargetRoom {
+	return MessageTargetRoom(id)
 }
